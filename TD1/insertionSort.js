2a6c1e044b241d0103ef7b5541b6c7ffd2d8142c
JA <jean-adam.puskaric@etu.unice.fr>
2018-09-26 10:04:25 +0200

    insertionSort

--- TD1/insertionSort.js
+++ TD1/insertionSort.js
@@ -1,4 +1,16 @@
+function insertionSort(array) {
+    insertionSortR(array, array.length-1);
+}
 
-function insertionSort (array) {
-    console.log("salut");
+function insertionSortR(array, n) {
+    if (n > 0) {
+        insertionSortR(array, n - 1);
+        let x = array[n];
+        let j = n-1;
+        while (j>=0 && array[j] > x) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = x;
+    }
 }
--- TD1/visualization.js
+++ TD1/visualization.js
@@ -1,8 +1,6 @@
 google.charts.load('current', {'packages': ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-insertionSort(0);
-
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Age', 'Weight'],
