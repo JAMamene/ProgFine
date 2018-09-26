9a50bc5647011c3eb90ac2966b653214cba73578
JA <jean-adam.puskaric@etu.unice.fr>
2018-09-26 17:59:52 +0200

    Plus dinfo mamane

--- TD1/utils.js
+++ TD1/utils.js
@@ -1,4 +1,4 @@
-if(Array.prototype.equals)
+if (Array.prototype.equals)
     console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
 Array.prototype.equals = function (array) {
     if (!array)
@@ -7,7 +7,7 @@ Array.prototype.equals = function (array) {
     if (this.length != array.length)
         return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
+    for (var i = 0, l = this.length; i < l; i++) {
         if (this[i] instanceof Array && array[i] instanceof Array) {
             if (!this[i].equals(array[i]))
                 return false;
@@ -20,8 +20,51 @@ Array.prototype.equals = function (array) {
 };
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
-function swap(array, i,j) {
+function swap(array, i, j) {
     let tmp = array[j];
     array[j] = array[i];
     array[i] = tmp;
+}
+
+function sparseIndex(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === "undefined") {
+            return i;
+        }
+    }
+    return false;
+}
+
+function standardDeviation(values) {
+    var avg = average(values);
+
+    var squareDiffs = values.map(function (value) {
+        var diff = value - avg;
+        return diff * diff;
+    });
+
+    var avgSquareDiff = average(squareDiffs);
+
+    return Math.sqrt(avgSquareDiff);
+}
+
+function average(data) {
+    var sum = data.reduce(function (sum, value) {
+        return sum + value;
+    }, 0);
+
+    return sum / data.length;
+}
+
+function statistics(data) {
+    return {
+        stdDev: standardDeviation(data),
+        avg: average(data),
+        min: data.reduce(function (a, b) {
+            return Math.min(a, b);
+        }),
+        max: data.reduce(function (a, b) {
+            return Math.max(a, b);
+        })
+    }
 }
--- TD1/visualization.js
+++ TD1/visualization.js
@@ -1,27 +1,53 @@
 google.charts.load('current', {'packages': ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-let ns = [1, 4, 16, 64, 256, 1024, 4096];
-let scores = new Array(ns.length);
-for (let i = 0; i < ns.length; i++) {
-    let datum = new Array(functions.length + 1);
-    datum[0] = Math.log2(ns[i]);
-    for (let k = 0; k < functions.length; k++) {
-        let times = new Array(256);
-        for (let j = 0; j < 256; j++) {
-            let tab = Array.from({length: ns[i]}, () => Math.floor(Math.random() * 1000));
-            let t0 = performance.now();
-            functions[k](tab);
-            let t1 = performance.now();
-            times[j] = (t1 - t0) * 1000;
+function measureTimes(alg, entrySize, iterations, maxMillis) {
+    let times = new Array(iterations);
+    for (let i = 0; i < iterations; i++) {
+        let tab = Array.from({length: entrySize}, () => Math.floor(Math.random() * 1000));
+        let t0 = performance.now();
+        alg(tab);
+        let t1 = performance.now();
+        if ((t1 - t0) > maxMillis) {
+            return null;
         }
-        let sum = times.reduce(function (a, b) {
-            return a + b;
-        });
-        let avg = sum / times.length;
-        datum[k + 1] = Math.log2(avg);
+        times[i] = (t1 - t0) * 1000;
     }
-    scores[i] = datum;
+    return times;
+}
+
+let ns = [4, 16, 64, 256, 512, 1024, 2048, 4096, 8196];
+let valuesScatter = new Array(ns.length);
+let candleCharts = new Array(functions.length);
+for (let i = 0; i < candleCharts.length; i++) {
+    candleCharts[i] = [];
+}
+
+for (let entrySizeIndex = 0; entrySizeIndex < ns.length; entrySizeIndex++) {
+    let datumScatter = new Array(functions.length + 1);
+    datumScatter[0] = Math.log2(ns[entrySizeIndex]);
+    for (let algIndex = 0; algIndex < functions.length; algIndex++) {
+        let datumCandle = new Array(5);
+        let times = measureTimes(functions[algIndex], ns[entrySizeIndex], 128, 128);
+        if (times == null) {
+            console.log(functions[algIndex].name + " FAILED at " + ns[entrySizeIndex]);
+        }
+        else {
+            let stats = statistics(times);
+
+            datumCandle[0] = "N=" + ns[entrySizeIndex];
+            let min = Math.log2(stats.min);
+            datumCandle[1] = min < 0 ? 0 : min;
+            let inf = Math.log2(stats.avg - stats.stdDev / 2);
+            datumCandle[2] = isNaN(inf) || inf < 0 ? 0 : inf;
+            datumCandle[3] = Math.log2(stats.avg + stats.stdDev / 2);
+            datumCandle[4] = Math.log2(stats.max);
+            candleCharts[algIndex][entrySizeIndex] = datumCandle;
+
+            datumScatter[algIndex + 1] = Math.log2(stats.avg);
+        }
+    }
+    valuesScatter[entrySizeIndex] = datumScatter;
 }
 
 function drawChart() {
@@ -30,47 +56,51 @@ function drawChart() {
     for (let i = 1; i <= functions.length; i++) {
         legend[i] = functions[i - 1].name;
     }
-    scores.unshift(legend);
-    console.log(scores);
-    let data = google.visualization.arrayToDataTable(
-        scores
+    valuesScatter.unshift(legend);
+    let dataScatter = google.visualization.arrayToDataTable(
+        valuesScatter
     );
-    let options = {
+    let trendline = {
+        type: 'linear',
+        showR2: true,
+        visibleInLegend: true
+    };
+    let optionsScatter = {
         chartArea: {width: '60%'},
         trendlines: {
-            0: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            },
-            1: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            },
-            2: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            },
-            3: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            },
-            4: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            },
-            5: {
-                type: 'linear',
-                showR2: true,
-                visibleInLegend: true
-            }
+            0: trendline,
+            1: trendline,
+            2: trendline,
+            3: trendline,
+            4: trendline,
+            5: trendline
         }
     };
 
     let chartLinear = new google.visualization.ScatterChart(document.getElementById('chartLinear'));
-    chartLinear.draw(data, options);
+    chartLinear.draw(dataScatter, optionsScatter);
+
+    for (let i = 0; i < functions.length; i++) {
+
+        let currentChart = candleCharts[i]
+
+        //handle a hole in the values
+        while (sparseIndex(currentChart) !== false) {
+            currentChart.splice(sparseIndex(currentChart), 1);
+        }
+
+        let dataCandle = google.visualization.arrayToDataTable(currentChart, true);
+
+        let optionsCandle = {
+            title: "Algorithm " + functions[i].name + " execution times",
+            legend: 'none'
+        };
+
+        let d1 = document.getElementById('chartLinear');
+        d1.insertAdjacentHTML('afterend', '<div id="chartCandle' + functions[i].name + '" style="height: 500px; width: 80%"></div>');
+
+        let chartCandle = new google.visualization.CandlestickChart(document.getElementById("chartCandle" + functions[i].name));
+
+        chartCandle.draw(dataCandle, optionsCandle);
+    }
 }
