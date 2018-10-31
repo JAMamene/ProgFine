6d3df250705f7fe487f293c4a25dbaa942dd8351
JA <jean-adam.puskaric@etu.unice.fr>
2018-10-31 12:12:02 +0100

    mvp

--- TD3/main.js
+++ TD3/main.js
@@ -1,11 +1,20 @@
 let canvas, context, lines;
 $(function () {
     canvas = $("#canvas")[0];
+    let size = 100;
     context = canvas.getContext('2d');
     lines = [];
-    lines.push(new Line(30, 40, 1, 180, 10, 5, 100));
-    lines.push(new Line(70, 100, 2, 90, 90, 25, 100));
-    lines.push(new Line(200, 384, 1, 240, 240, -2, 100));
+    for (let i = 0; i < 100; i++) {
+        lines.push(new Line(
+            Math.floor(Math.random() * (canvas.width - size) + size),
+            Math.floor(Math.random() * (canvas.height - size) + size),
+            Math.floor(Math.random() * 2),
+            Math.floor(Math.random() * 360),
+            Math.floor(Math.random() * 360),
+            Math.floor((Math.random() * 10) - 5),
+            size
+        ));
+    }
     window.requestAnimationFrame(update);
 });
 
@@ -15,6 +24,15 @@ function update() {
         line.update(context);
     });
     window.requestAnimationFrame(update);
-
+    for (let i = 0; i < lines.length; i++) {
+        for (let j = i; j < lines.length; j++) {
+            let point = (lines[i].intersect(lines[j]));
+            if (point) {
+                context.rect(point.x-1, point.y-1, 2, 2);
+                context.fillStyle = "red";
+                context.fill();
+            }
+        }
+    }
 }
 
