cc7d5d5b93ac84ef03ff2661ee6c734f35113480
JA <jean-adam.puskaric@etu.unice.fr>
2018-10-31 11:36:52 +0100

    mini push

--- TD3/main.js
+++ TD3/main.js
@@ -1,21 +1,20 @@
-let canvas, context, line, line1, line2;
+let canvas, context, lines;
 $(function () {
     canvas = $("#canvas")[0];
     context = canvas.getContext('2d');
-    line = new Line(30, 40, 1, 180, 10, 5, 30);
-    line1 = new Line(70, 100, 2, 90, 90, 25, 40);
-    line2 = new Line(200, 384, 1, 240, 240, -2, 300);
-
+    lines = [];
+    lines.push(new Line(30, 40, 1, 180, 10, 5, 100));
+    lines.push(new Line(70, 100, 2, 90, 90, 25, 100));
+    lines.push(new Line(200, 384, 1, 240, 240, -2, 100));
     window.requestAnimationFrame(update);
 });
 
 function update() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    line.update(context);
-    line1.update(context);
-    line2.update(context);
+    lines.forEach(line => {
+        line.update(context);
+    });
     window.requestAnimationFrame(update);
 
 }
 
-// Longuer = 1.4/racine(n)
