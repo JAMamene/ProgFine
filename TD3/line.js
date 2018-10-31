8b33f94c19388f511305b168404a0c770fd7f992
Guillaume André <guillaume.andre@etu.unice.fr>
2018-10-31 11:41:39 +0100

    add points

--- TD3/line.js
+++ TD3/line.js
@@ -1,29 +1,49 @@
 Line = function (centerX, centerY, speed, angle, rotation, rotationAngle, size) {
-    this.centerX = centerX;
-    this.centerY = centerY;
+    this.center = new Point(centerX, centerY);
     this.speed = speed;
     this.angle = Math.PI * angle / 180.0;
     this.rotation = Math.PI * rotation / 180.0;
     this.rotationAngle = Math.PI * rotationAngle / 180.0;
     this.size = size;
+    this.firstEnd = new Point();
+    this.secondEnd = new Point();
+};
+
+Point = function (x, y) {
+    this.x = x;
+    this.y = y;
 };
 
 Line.prototype.draw = function (ctx) {
     ctx.beginPath();
-    ctx.moveTo(this.centerX, this.centerY);
-    ctx.lineTo(
-        this.centerX + this.size / 2 * Math.cos(this.rotation), this.centerY + this.size / 2 * Math.sin(this.rotation)
-    );
-    ctx.moveTo(this.centerX, this.centerY);
-    ctx.lineTo(
-        this.centerX - this.size / 2 * Math.cos(this.rotation), this.centerY - this.size / 2 * Math.sin(this.rotation)
-    );
+    ctx.moveTo(this.center.x, this.center.y);
+    this.updateFirstEnd();
+    this.updateSecondEnd();
+    ctx.lineTo(this.firstEnd.x, this.firstEnd.y);
+    ctx.moveTo(this.center.x, this.center.y);
+    ctx.lineTo(this.secondEnd.x, this.secondEnd.y);
     ctx.stroke();
+    this.checkForCollision(ctx);
 };
 
+Line.prototype.updateFirstEnd = function () {
+    this.firstEnd.x = this.center.x + this.size / 2 * Math.cos(this.rotation);
+    this.firstEnd.y = this.center.y + this.size / 2 * Math.sin(this.rotation);
+};
+
+Line.prototype.updateSecondEnd = function () {
+    this.secondEnd.x = this.center.x - this.size / 2 * Math.cos(this.rotation);
+    this.secondEnd.y = this.center.y - this.size / 2 * Math.sin(this.rotation);
+};
+
+Line.prototype.checkForCollision = function (ctx) {
+
+};
+
+
 Line.prototype.update = function (ctx) {
-    this.centerX += this.speed * Math.cos(this.angle);
-    this.centerY += this.speed * Math.sin(this.angle);
+    this.center.x += this.speed * Math.cos(this.angle);
+    this.center.y += this.speed * Math.sin(this.angle);
     this.rotation += this.rotationAngle;
     this.draw(ctx);
 };
