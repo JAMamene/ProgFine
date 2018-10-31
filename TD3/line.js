b8fdfc19279b511828a12dd25a0526309cae5b29
JA <jean-adam.puskaric@etu.unice.fr>
2018-10-31 11:19:19 +0100

    mini push

--- TD3/line.js
+++ TD3/line.js
@@ -1,21 +1,27 @@
-Line = function (centerX, centerY, speed, angle, rotation, size) {
+Line = function (centerX, centerY, speed, angle, rotation, rotationAngle, size) {
     this.centerX = centerX;
     this.centerY = centerY;
     this.speed = speed;
-    this.angle = angle;
-    this.rotation = rotation;
+    this.angle = Math.PI * angle / 180.0;
+    this.rotation = Math.PI * rotation / 180.0;
+    this.rotationAngle = Math.PI * rotationAngle / 180.0;
     this.size = size;
 };
 
 Line.prototype.draw = function (ctx) {
     ctx.moveTo(this.centerX, this.centerY);
     ctx.lineTo(
-        this.centerX + this.size / 2 * Math.cos(Math.PI * this.angle / 180.0), this.centerY + this.size / 2 * Math.sin(Math.PI * this.angle / 180)
+        this.centerX + this.size / 2 * Math.cos(this.rotation), this.centerY + this.size / 2 * Math.sin(this.rotation)
     );
     ctx.moveTo(this.centerX, this.centerY);
     ctx.lineTo(
-        this.centerX - this.size / 2 * Math.cos(Math.PI * this.angle / 180.0), this.centerY - this.size / 2 * Math.sin(Math.PI * this.angle / 180)
+        this.centerX - this.size / 2 * Math.cos(this.rotation), this.centerY - this.size / 2 * Math.sin(this.rotation)
     );
-    ctx.fillRect(this.centerX, this.centerY, 3, 3);
     ctx.stroke();
+};
+
+Line.prototype.update = function () {
+    this.centerX += this.angle * this.speed;
+    this.centerY += this.angle * this.speed;
+    this.rotation += this.rotationAngle;
 };
--- TD3/main.js
+++ TD3/main.js
@@ -1,12 +1,17 @@
 $(function () {
     let canvas = $("#canvas")[0];
     let context = canvas.getContext('2d');
-    let line = new Line(30, 40, null, 180, null, 100);
-    let line1 = new Line(70, 100, null, 90, null, 40);
-    let line2 = new Line(200, 384, null, 240, null, 300);
+    let line = new Line(30, 40, 1, 180, 10, 5, 30);
+    let line1 = new Line(70, 100, 2, 90, 90, 25, 40);
+    let line2 = new Line(200, 384, 3, 240, 100, 32, 300);
     line.draw(context);
     line1.draw(context);
     line2.draw(context);
+    window.requestAnimationFrame(update);
 });
+
+function update() {
+
+}
 
 // Longuer = 1.4/racine(n)
